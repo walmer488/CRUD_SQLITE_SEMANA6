6eac3887d09bf34ba067915f8d67735b7e47dd4b
Walmer <walmermartinez168@gmail.com>
2020-09-30 20:38:48 -0300

    Creación Interfaz Detelles_Artículos

---
+++ app/src/main/java/com/example/crud_sqlite_sem6/detalle_articulos.java
@@ -0,0 +1,14 @@
+package com.example.crud_sqlite_sem6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detalle_articulos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_articulos);
+    }
+}
