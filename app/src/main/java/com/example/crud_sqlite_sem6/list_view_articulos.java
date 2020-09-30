675e2649a816b29c39b6420b878f86b36abf87f1
Walmer <walmermartinez168@gmail.com>
2020-09-30 20:26:55 -0300

    Creación Interfaz List_View_Articulos

---
+++ app/src/main/java/com/example/crud_sqlite_sem6/list_view_articulos.java
@@ -0,0 +1,14 @@
+package com.example.crud_sqlite_sem6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class list_view_articulos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view_articulos);
+    }
+}
