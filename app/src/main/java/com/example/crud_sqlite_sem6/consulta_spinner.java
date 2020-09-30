c4ff2d11f4b46980350fb3fbf7954a1562e6b949
Walmer <walmermartinez168@gmail.com>
2020-09-30 20:21:48 -0300

    Creación Interfaz Consulta_Spinner

---
+++ app/src/main/java/com/example/crud_sqlite_sem6/consulta_spinner.java
@@ -0,0 +1,14 @@
+package com.example.crud_sqlite_sem6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class consulta_spinner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_spinner);
+    }
+}
