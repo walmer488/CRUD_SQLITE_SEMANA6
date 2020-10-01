9858572df9352130a72b821bb938caa6f2592ad1
Walmer <walmermartinez168@gmail.com>
2020-09-30 21:28:51 -0300

    Codificacion de la clase list_view_articulos

--- app/src/main/java/com/example/crud_sqlite_sem6/list_view_articulos.java
+++ app/src/main/java/com/example/crud_sqlite_sem6/list_view_articulos.java
@@ -1,14 +1,77 @@
 package com.example.crud_sqlite_sem6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class list_view_articulos extends AppCompatActivity {
+
+    android.widget.ListView listViewPersonas;
+    ArrayAdapter adaptador;
+    SearchView searchView;
+    ListView ListView;
+    ArrayList<String> list;
+    ArrayAdapter adapter;
+
+    ConexionSQLite conexion = new ConexionSQLite(this);
+    Dto datos = new Dto();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view_articulos);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        listViewPersonas= (ListView) findViewById(R.id.listViewPersonas);
+        searchView = (SearchView) findViewById(R.id.searchView);
+
+        adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1, conexion.consultaListaArticulos1());
+        listViewPersonas.setAdapter(adaptador);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                String text = s;
+                adaptador.getFilter().filter(text);
+                return false;
+            }
+        });
+
+
+        listViewPersonas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+
+                String informacion="Código: "+conexion.consultaListaArticulos().get(pos).getCodigo()+"\n";
+                informacion+="Descripcién: "+conexion.consultaListaArticulos().get(pos).getDescripcion()+"\n";
+                informacion+="Precio: "+conexion.consultaListaArticulos().get(pos).getPrecio();
+
+                Dto articulos = conexion.consultaListaArticulos().get(pos);
+                Intent intent = new Intent(list_view_articulos.this, detalle_articulos.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("articulo", articulos);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
     }
 }
